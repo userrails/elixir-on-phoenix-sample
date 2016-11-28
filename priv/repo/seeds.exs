c3d20c1a8fac810fff54c6e6dcb16c622851ab2a
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-11-28 22:45:33 +0545

    seed data loaded

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,5 +10,11 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-post = %HelloWorld.Post{title: "title", body: "deadfda"}
-HelloWorld.Repo.insert(post)
+
+#post = %HelloWorld.Post{title: "title", body: "deadfda"}
+#HelloWorld.Repo.insert(post)
+
+for i <- 1..78300 do 
+	post = %HelloWorld.Post{title: "titafsdfdsleasdfdfsfds#{i}#{i}", body: "this is realadfdfsdsfsdffdly a descriptaadfsdsfdasfdfion#{i}#{i}"}
+    HelloWorld.Repo.insert(post)
+end
