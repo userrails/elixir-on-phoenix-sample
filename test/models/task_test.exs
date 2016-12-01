7ebad3caa7f143deac5f11e817cdc3014683310a
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-12-01 17:54:28 +0545

    test cases task has many comments added

--- test/models/task_test.exs
+++ test/models/task_test.exs
@@ -1,9 +1,11 @@
+require IEx
 defmodule HelloWorld.TaskTest do
   use HelloWorld.ModelCase
 
   alias HelloWorld.Task
+  # alias HelloWorld.Comment
 
-  @valid_attrs %{body: "some content", name: "some content"}
+  @valid_attrs %{body: "This is body of my task", name: "My first task"}
   @invalid_attrs %{}
 
   test "changeset with valid attributes" do
@@ -14,5 +16,19 @@ defmodule HelloWorld.TaskTest do
   test "changeset with invalid attributes" do
     changeset = Task.changeset(%Task{}, @invalid_attrs)
     refute changeset.valid?
+  end
+
+  test "Task should have many comments" do
+    task_changeset = Task.changeset(%Task{}, @valid_attrs)
+    {status, task} = Repo.insert(task_changeset)  
+    
+    comment_changeset1 = HelloWorld.Comment.changeset(%HelloWorld.Comment{body: "This is awesome task1!", task_id: task.id})
+    {status, comment1} = Repo.insert(comment_changeset1)
+
+    comment_changeset2 = HelloWorld.Comment.changeset(%HelloWorld.Comment{body: "This is awesome task2!", task_id: task.id})
+    {status, comment2} = Repo.insert(comment_changeset2)
+
+    task_comments = HelloWorld.Repo.get(HelloWorld.Task, task.id) |> HelloWorld.Repo.preload(:comments)
+    assert task_comments.comments == [comment1, comment2]
   end
 end
