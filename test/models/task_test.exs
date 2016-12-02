19967fe163b568afceefbc172fc763d87f340ea0
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-12-02 13:12:09 +0545

    should all task's comments on task deleted

--- test/models/task_test.exs
+++ test/models/task_test.exs
@@ -31,4 +31,19 @@ defmodule HelloWorld.TaskTest do
     task_comments = HelloWorld.Repo.get(HelloWorld.Task, task.id) |> HelloWorld.Repo.preload(:comments)
     assert task_comments.comments == [comment1, comment2]
   end
+
+  test "it should delete all dependent comments when task is deleted" do
+    task_changeset = Task.changeset(%Task{}, @valid_attrs)
+    {status, task} = Repo.insert(task_changeset)
+
+    comment_changeset1 = HelloWorld.Comment.changeset(%HelloWorld.Comment{body: "This is awesome task1!", task_id: task.id})
+    {status, comment1} = Repo.insert(comment_changeset1)
+
+    HelloWorld.Repo.delete(task)
+    tsk = HelloWorld.Repo.get(HelloWorld.Task, task.id)
+    cmt = HelloWorld.Repo.get(HelloWorld.Comment, comment1.id)
+
+    assert tsk == nil
+    assert cmt == nil
+  end
 end
