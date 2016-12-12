cd20ca195184fbc7fc95d4d97248a57b2a93ae65
Shiv Raj Badu <shivrajbadu@gmail.com>
2016-12-12 13:07:49 +0545

    small changes

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -8,8 +8,6 @@ defmodule HelloWorld.SearchController do
 		 column = search_params["column"]
 		 keyword = search_params["keyword"]
 
-		 
-
 		 cond do
 		 	column == "id" -> query = from p in HelloWorld.Post, where: p.id == ^keyword, select: p
 		 	column == "title" -> query = from p in HelloWorld.Post, where: p.title == ^keyword, select: p
